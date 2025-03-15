26c14dd291a7af3e6228484d501b50a7109c5f81
pedroaltobelli23 <pedroaltobelli673@gmail.com>
2025-03-15 19:34:22 -0300

    frontend

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # color-finder-extension
 Google Chrome extension for color identification on websites
+
+
+# Reference
+- https://github.com/simov/screenshot-capture
---
+++ frontend/index.js
@@ -0,0 +1,39 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "inject") {
+      inject(message.tabId);
+  }
+});
+
+function inject(tabId) {
+  chrome.tabs.sendMessage(tabId, { message: "init" }, (res) => {
+    if (res) {
+      clearTimeout(timeout);
+    }
+  });
+
+  var timeout = setTimeout(() => {
+      chrome.scripting.insertCSS({ files: ["vendor/jquery.Jcrop.min.css"], target: { tabId } });
+      chrome.scripting.insertCSS({ files: ["content/index.css"], target: { tabId } });
+
+      chrome.scripting.executeScript({ files: ["vendor/jquery.min.js"], target: { tabId } });
+      chrome.scripting.executeScript({ files: ["vendor/jquery.Jcrop.min.js"], target: { tabId } });
+      chrome.scripting.executeScript({ files: ["content/crop.js"], target: { tabId } });
+      chrome.scripting.executeScript({ files: ["content/index.js"], target: { tabId } });
+
+      setTimeout(() => {
+          chrome.tabs.sendMessage(tabId, { message: "init" });
+      }, 100);
+  }, 100);
+}
+
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+  if (req.message === 'capture') {
+    chrome.tabs.query({active: true, currentWindow: true}, (tab) => {
+      chrome.tabs.captureVisibleTab(tab.windowId, {format: req.format, quality: req.quality}, (image) => {
+        // image is base64
+        res({message: 'image', image})
+      })
+    })
+  }
+  return true
+})
